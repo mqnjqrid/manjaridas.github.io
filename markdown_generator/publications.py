d1817e3bfdfd27fdd8545778c60897c6fc178eb5
manjarid <manjarid@andrew.cmu.edu>
2021-11-18 09:53:17 -0500

    Update publications.py

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -80,8 +80,9 @@ for row, item in publications.iterrows():
         md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
     
     md += "\ndate: " + str(item.pub_date) 
-    
-    md += "\nvenue: '" + html_escape(item.venue) + "'"
+   
+    if len(str(item.venue)) > 1:
+        md += "\nvenue: '" + html_escape(item.venue) + "'"
     
     if len(str(item.paper_url)) > 5:
         md += "\npaperurl: '" + item.paper_url + "'"
